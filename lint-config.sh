74ffe6f04e654c8e26e60b788120b6aded16f4bf
Anthony Spruyt <aspruyt@hotmail.co.uk>
2026-09-15 06:11:25 +0000

    chore(lint): update megalinter to latest

--- lint-config.sh
+++ lint-config.sh
@@ -5,7 +5,7 @@
 
 # MegaLinter Docker image (use digest for reproducibility)
 # renovate: datasource=docker depName=ghcr.io/anthony-spruyt/megalinter-container-images
-MEGALINTER_IMAGE="ghcr.io/anthony-spruyt/megalinter-container-images:v10.0.43@sha256:d70e8d9e3c7773eb1b4aee47103b9760206e58d14c6e4ef71ee785ef0926f56d"
+MEGALINTER_IMAGE="ghcr.io/anthony-spruyt/megalinter-container-images:latest"
 
 # Skip linting for renovate/dependabot commits in CI
 SKIP_BOT_COMMITS=false
